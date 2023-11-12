b2ac8fe5bfd4b7a51c4e456423b20307454c0ec6
khizeryz14 <124634151+khizeryz14@users.noreply.github.com>
2023-11-13 02:54:39 +0500

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,14 @@
+myList = [1,3,5,1,2,19,22,13,16,99,24,51]
+pos = 0
+for k in range(len(myList)):
+    smallest  = myList[k]
+    for i in range(k,len(myList)):
+        if smallest > myList[i]:
+            smallest = myList[i]
+            pos = i
+    temp = myList[pos]
+    myList[pos] = myList[k]
+    myList[k] = temp
+pos = k 
+
+print(myList)
